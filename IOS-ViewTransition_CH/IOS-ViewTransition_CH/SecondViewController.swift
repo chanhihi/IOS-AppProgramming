d3296a1c624b426fa43ad1d19e6b4995b1531bef
chanhihi <chanhihi55@gmail.com>
2021-10-26 01:40:55 +0900

    View 상태 변화 메서드
    https://developer.apple.com/documentation/uikit/uiviewcontroller
    
    UI view Controller

--- IOS-ViewTransition_CH/IOS-ViewTransition_CH/SecondViewController.swift
+++ IOS-ViewTransition_CH/IOS-ViewTransition_CH/SecondViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        print("SecondViewController의 View가 메모리에 로드")
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("SecondViewController의 View가 화면에 보여질 예정")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("SecondViewController의 View가 화면에 보여짐")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("SecondViewController의 View가 화면에서 사라질 예정")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("SecondViewController의 View가 화면에서 사라짐")
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        print("SecondViewController의 View가 subview를 레이아웃 하려함")
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        print("SecondViewController의 View가 subview를 레이아웃 함")
+    }
+
     
     @IBAction func popToPrev() {
         self.navigationController?.popViewController(animated: true)
--- IOS-ViewTransition_CH/IOS-ViewTransition_CH/ViewController.swift
+++ IOS-ViewTransition_CH/IOS-ViewTransition_CH/ViewController.swift
@@ -12,8 +12,39 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("ViewController의 View가 메모리에 로드")
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("ViewController의 View가 화면에 보여질 예정")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("ViewController의 View가 화면에 보여짐")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("ViewController의 View가 화면에서 사라질 예정")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("ViewController의 View가 화면에서 사라짐")
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        print("ViewController의 View가 subview를 레이아웃 하려함")
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        print("ViewController의 View가 subview를 레이아웃 함")
+    }
 
 }
 
