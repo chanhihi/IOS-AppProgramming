6e2766fac75b023f94efd8b8d2e3d30796f1f4b0
chanhihi <chanhihi55@gmail.com>
2021-11-06 17:11:01 +0900

    DatePicker Target-Action
    Target Action 예제

---
+++ IOS-DatePicker_CH/IOS-DatePicker_CH/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  IOS-DatePicker_CH
+//
+//  Created by Chan on 2021/11/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var dateLabel: UILabel!
+    let dateFormatter: DateFormatter = {
+        let formatter: DateFormatter = DateFormatter()
+//        formatter.dateStyle = .medium
+//        formatter.timeStyle = .medium
+        formatter.dateFormat = "yyyy/MM/dd hh:mm:ss"
+        return formatter
+    }()
+    
+    @IBAction func didDatePickerValueChanged(_ sender: UIDatePicker) {
+        print("value changed")
+        
+//        let date: Date = sender.date
+        let date: Date = self.datePicker.date
+        let dateString: String = self.dateFormatter.string(from: date)
+        
+        self.dateLabel.text = dateString
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        self.datePicker.addTarget(self, action: #selector(self.didDatePickerValueChanged(_:)), for: UIControl.Event.valueChanged)
+    }
+
+
+}
+
