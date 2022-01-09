3ba8baa6cbd4c4171c4f739001d492d33189c986
chanhihi <chanhihi55@gmail.com>
2022-01-09 18:18:28 +0900

    create sololearn prj

---
+++ IOS-Sololearn_CH/IOS-Sololearn_CH/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  IOS-Sololearn_CH
+//
+//  Created by Chan on 2022/01/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func nameTextField(_ sender: UITextField) {
+        
+    }
+    
+}
+
